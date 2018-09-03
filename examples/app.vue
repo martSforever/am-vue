1ebb4b4f19075074924e45e08b6af7b8c17ae6c5
martsforever <martsforever@aliyun.com>
2018-09-03 11:22:31 +0800

    Initial project learn from iview

---
+++ examples/app.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        this is app
+    </div>
+</template>
+<script>
+    module.exports = {
+        data: function () {
+            return {}
+        },
+        mounted: function () {
+
+        },
+        beforeDestroy: function () {
+
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="less" scoped>
+    nav {
+        margin-bottom: 40px;
+
+        ul {
+            display: flex;
+            flex-wrap: wrap;
+        }
+        li {
+            display: inline-block;
+        }
+        li + li {
+            border-left: solid 1px #bbb;
+            padding-left: 10px;
+            margin-left: 10px;
+        }
+    }
+
+    .container {
+        padding: 10px 40px 0;
+    }
+
+    .v-link-active {
+        color: #bbb;
+    }
+</style>
