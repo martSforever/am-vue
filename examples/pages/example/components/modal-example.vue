bcfb4359e9f4b800db13a41f611e515dcafbacd1
martSforever <marts_forever@163.com>
2018-09-16 23:42:53 +0800

    示例代码调整

--- examples/pages/example/components/modal-example.vue
+++ examples/pages/example/components/modal-example.vue
@@ -2,9 +2,16 @@
     <div class="modal-example example-page">
         <div class="title">基本用法</div>
         <div class="example-row">
-            <am-button @click="show1 = !show1">toggle[{{show1}}]</am-button>
+            <am-button @click="show1 = !show1">normal</am-button>
+            <am-modal v-model="show1" title="通知标题" message="通知内容"/>
         </div>
+        <!--<div class="title">无遮罩</div>
         <div class="example-row">
+            <am-button @click="show2 = !show2">toggle[{{show2}}]</am-button>
+            <am-modal v-model="show2" title="通知标题" message="通知内容" :shadow="false"/>
+        </div>-->
+
+        <!--<div class="example-row">
             <am-button-group>
                 <am-button label="primary" @click="(type='primary') && (show1 = true)" color="primary"/>
                 <am-button label="info" @click="(type='info') && (show1 = true)" color="info"/>
@@ -27,19 +34,31 @@
                   top="10vh"
         >
 
-        </am-modal>
+        </am-modal>-->
     </div>
 </template>
 
 <script>
     import AmButtonGroup from "../../../../src/components/am-button/am-button-group";
+    import AmModal from "../../../../src/components/am-modal/am-modal";
 
     export default {
         name: 'modal-example',
-        components: {AmButtonGroup},
+        components: {AmModal, AmButtonGroup},
         data() {
             return {
                 show1: false,
+                show2: false,
+                show3: false,
+                show4: false,
+                show5: false,
+                show6: false,
+                show7: false,
+                show8: false,
+                show9: false,
+                show10: false,
+
+
                 type: 'info'
             };
         },
