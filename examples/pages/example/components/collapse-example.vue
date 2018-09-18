99d8091b9224fabd6e3f9c3aa71495e5ec6c0801
martsforever <martsforever@aliyun.com>
2018-09-18 09:23:47 +0800

    collapse example

--- examples/pages/example/components/collapse-example.vue
+++ examples/pages/example/components/collapse-example.vue
@@ -3,8 +3,6 @@
         <div class="title">普通用法</div>
         <div class="example-row">
             <am-button label="toggle" @click="val1 = !val1"/>
-            <am-button label="add" @click="list.push(list.length)"/>
-            <am-button label="remove" @click="list.pop()"/>
         </div>
         <div class="example-row" style="width: 500px">
             <am-collapse v-model="val1" icon-color="white">
@@ -17,32 +15,49 @@
                 </div>
             </am-collapse>
         </div>
-
-        <!--
-             <div class="test-collapse-item"
-                     v-for="item in list">
-                    {{item}}
+        <div class="title">折叠动画不会影响原来容器的宽高</div>
+        <div class="example-row">
+            <div class="example-row">
+                <am-button label="toggle" @click="val2 = !val2"/>
+                <am-button label="add" @click="list.push(list.length)"/>
+                <am-button label="remove" @click="list.pop()"/>
+            </div>
+        </div>
+        <div class="example-row" style="width: 500px">
+            <am-collapse v-model="val2" icon-color="white">
+                <div slot="head"
+                     style="background-color: blueviolet;height: 44px;line-height: 44px;padding: 0 1em;color: white">
+                    this is head
                 </div>
-            -->
+                <div style="background-color: salmon">
+                    <div class="test-collapse-item"
+                         v-for="item in list">
+                        {{item}}
+                    </div>
+                </div>
+            </am-collapse>
+        </div>
+
     </div>
 </template>
 
 <script>
     export default {
-        name: "collapse-example",
+        name: 'collapse-example',
         data() {
             return {
-                val1: true,
+                val1: false,
+                val2: false,
                 list: [1, 2, 3, 4],
-            }
+            };
         },
-    }
+    };
 </script>
 
 <style lang="scss">
     .test-collapse-item {
         height: 50px;
-        width: 600px;
+        width: 100%;
         background-color: blueviolet;
         color: white;
         display: block;
