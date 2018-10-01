c0deebc1eac48f752b314ee067a435b174b83b54
martSforever <marts_forever@163.com>
2018-10-01 22:45:48 +0800

    先检查一遍在ie下tab组件的indicator为什么没有移动

--- examples/pages/example/components/scrollbar-example.vue
+++ examples/pages/example/components/scrollbar-example.vue
@@ -1,17 +1,57 @@
 <template>
     <div class="scrollbar-example example-page">
         <am-scrollbar>
-
+            <am-button-group>
+                <am-button @click="add">add</am-button>
+                <am-button>{{list.length}}</am-button>
+                <am-button @click="remove">remove</am-button>
+            </am-button-group>
+            <div class="item" v-for="item in list">
+                <img :src="item">
+            </div>
         </am-scrollbar>
     </div>
 </template>
 
 <script>
+
+    const imageUrl = 'http://www.pujia8.com/static/pics/20171215040134_26.jpg';
+    const image2 = 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=902852676,3612482722&fm=26&gp=0.jpg'
+
     export default {
-        name: "scrollbar-example"
+        name: "scrollbar-example",
+        data() {
+            return {
+                list: []
+            }
+        },
+        mounted() {
+            let i = 1;
+            while (i > 0) {
+                this.list.push(image2)
+                i--
+            }
+        },
+        methods: {
+            add() {
+                this.list.unshift(`${image2}?time=${this.getTime()}`)
+            },
+            remove() {
+                this.list.shift()
+            },
+            getTime() {
+                return new Date().getTime();
+            },
+        },
     }
 </script>
 
 <style lang="scss">
-
+    .scrollbar-example {
+        .item {
+            background-color: #f2f2f2;
+            color: black;
+            margin-bottom: 12px;
+        }
+    }
 </style>
--- src/components/am-scrollbar/am-scrollbar.vue
+++ src/components/am-scrollbar/am-scrollbar.vue
@@ -1,11 +1,30 @@
 <template>
     <div class="am-scrollbar">
-        scrollbar
+        <div class="am-scrollbar-content-wrapper" ref="content">
+            <span>width：{{width}}，height:{{height}}</span>
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
+    const elementResizeDetectorMaker = require("element-resize-detector");
+    let erdUltraFast = elementResizeDetectorMaker({strategy: "scroll"});
+
     export default {
-        name: "am-scrollbar"
+        name: "am-scrollbar",
+        data() {
+            return {
+                width: 0,
+                height: 0,
+            }
+        },
+        mounted() {
+            erdUltraFast.listenTo(this.$refs.content, (element) => {
+                console.log('resize')
+                this.width = element.offsetWidth;
+                this.height = element.offsetHeight;
+            });
+        },
     }
 </script>
