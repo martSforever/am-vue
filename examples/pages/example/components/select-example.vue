b5c9687454bd4ecccd4992bf8762039fcd6b19ad
martSforever <marts_forever@163.com>
2018-10-05 14:50:11 +0800

    select example data

--- examples/pages/example/components/select-example.vue
+++ examples/pages/example/components/select-example.vue
@@ -2,7 +2,10 @@
     <div class="select-example example-page">
         <div class="title">基本用法</div>
         <div class="example-row">
-            <am-select :data="list" :show="true" children-key="children">
+            <am-select :data="list"
+                       children-key="children"
+                       @select="handleSelect"
+            >
                 <template slot-scope="data">
                     {{data.name}}
                 </template>
@@ -28,27 +31,31 @@
                                     {name: '顺德区',}
                                 ]
                             },
-                            {name: '广州市广州市'},
-                            {name: '深圳市'},
-                            {
-                                name: '佛山市', children: [
-                                    {name: '禅城区',},
-                                    {name: '顺德区',}
-                                ]
-                            },
-                            {name: '广州市广州市'},
-                            {name: '深圳市'},
+                            {name: '广州市', children: [{name: '越秀区'}, {name: '天河区'}, {name: '白云区'}]},
+                            {name: '深圳市', children: [{name: '龙岗区'}, {name: '宝安区'}]},
+                            {name: '韶关市', children: [{name: '武江区'}, {name: '仁化县'}]},
+                            {name: '江门市', children: [{name: '蓬江区'}, {name: '大埔县'}]},
                         ]
                     },
                     {
                         name: '广西省', children: [
-                            {name: '南宁市'},
-                            {name: '贵港市'},
-                            {name: '防城港'},
+                            {name: '南宁市', children: [{name: '江南区'}, {name: '马山县'}, {name: '横县'},]},
+                            {name: '贵港市', children: [{name: '桂平县'}, {name: '平南县'},]},
+                            {name: '防城港市', children: [{name: '港口区'}]},
                         ]
                     },
                 ]
             }
+        },
+        methods: {
+            handleSelect(result) {
+                this.$notice.show({
+                    message: result.reduce((ret, item) => {
+                        ret += item.name
+                        return ret
+                    }, '')
+                })
+            },
         },
     }
 </script>
--- src/components/am-select/am-select-item.vue
+++ src/components/am-select/am-select-item.vue
@@ -8,6 +8,7 @@
             :scrollbar="false"
         >
             <div class="am-select-item-content"
+                 @click="handleClick"
                  slot="reference">
                 <am-rendering-scope-slot :scope-slot-func="scopeSlotFunc" :data="data" v-if="!!scopeSlotFunc"/>
                 <am-rendering-render-func :render-func="renderingRenderFunc" :data="data" v-if="!!renderingRenderFunc"/>
@@ -34,6 +35,7 @@
     import AmRenderingRenderFunc from '../am-render/rendering-render-func'
     import AmDropdown from '../am-dropdown'
     import AmPopover from '../am-popover'
+    import {findComponentUpward} from "../../scripts/dom";
 
     export default {
         name: "am-select-item",
@@ -52,14 +54,36 @@
         data() {
             return {
                 currentValue: false,
+                dropdown: null,
             }
+        },
+        mounted() {
+            this.dropdown = this.$refs.dropdown
         },
         methods: {
             handleMouseEnter() {
                 this.$nextTick(() => {
-                    clearTimeout(this.$refs.dropdown.popoverTimer)
-                    this.$refs.dropdown.popoverTimer = null
+                    clearTimeout(this.dropdown.popoverTimer)
+                    this.dropdown.popoverTimer = null
                 })
+            },
+            handleClick() {
+                let result = []
+                result.unshift(this.data)
+                this.dropdown.referenceHover = false
+                this.dropdown.popoverHover = false
+                let parent = findComponentUpward(this, 'am-select-item')
+                while (parent != null) {
+                    result.unshift(parent.data)
+                    parent.dropdown.referenceHover = false
+                    parent.dropdown.popoverHover = false
+                    parent = findComponentUpward(parent, 'am-select-item')
+                }
+                let root = findComponentUpward(this, 'am-select')
+                if (!!root) {
+                    root.$emit('select', result)
+                    root.currentShow = false
+                }
             },
         },
     }
