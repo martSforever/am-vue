226756ddda9f55222e5060164c8b87e592775c33
martSforever <marts_forever@163.com>
2018-10-05 13:16:31 +0800

    fix select-item

--- src/components/am-select/am-select-item.vue
+++ src/components/am-select/am-select-item.vue
@@ -1,5 +1,28 @@
 <template>
     <div class="am-select-item">
+        <!--<div class="am-select-item-content"
+             @click="currentValue = !currentValue"
+             ref="reference">
+            <am-rendering-scope-slot :scope-slot-func="scopeSlotFunc" :data="data" v-if="!!scopeSlotFunc"/>
+            <am-rendering-render-func :render-func="renderingRenderFunc" :data="data" v-if="!!renderingRenderFunc"/>
+        </div>
+        <am-popover parent-name="am-select-item"
+                    reference-name="reference"
+                    direction="right"
+                    v-model="currentValue"
+        >
+            <div style="width: 50px;height: 50px">
+                <am-select-item
+                    v-for="(item,index) in data[childrenKey]"
+                    :key="index"
+                    :scope-slot-func="scopeSlotFunc"
+                    :rendering-render-func="renderingRenderFunc"
+                    :data="item"
+                    :children-key="childrenKey"
+                />
+            </div>
+        </am-popover>-->
+
         <am-dropdown
             trigger="click"
             direction="right"
@@ -11,14 +34,14 @@
                 <am-rendering-render-func :render-func="renderingRenderFunc" :data="data" v-if="!!renderingRenderFunc"/>
             </div>
             <div slot="popover">
-                    <am-select-item
-                        v-for="(item,index) in data[childrenKey]"
-                        :key="index"
-                        :scope-slot-func="scopeSlotFunc"
-                        :rendering-render-func="renderingRenderFunc"
-                        :data="item"
-                        :children-key="childrenKey"
-                    />
+                <am-select-item
+                    v-for="(item,index) in data[childrenKey]"
+                    :key="index"
+                    :scope-slot-func="scopeSlotFunc"
+                    :rendering-render-func="renderingRenderFunc"
+                    :data="item"
+                    :children-key="childrenKey"
+                />
             </div>
         </am-dropdown>
     </div>
@@ -29,10 +52,16 @@
     import AmRenderingScopeSlot from '../am-render/rendering-scope-slot'
     import AmRenderingRenderFunc from '../am-render/rendering-render-func'
     import AmDropdown from '../am-dropdown'
+    import AmPopover from '../am-popover'
 
     export default {
         name: "am-select-item",
-        components: {AmRenderingScopeSlot, AmRenderingRenderFunc, AmDropdown},
+        components: {
+            AmRenderingScopeSlot,
+            AmRenderingRenderFunc,
+            AmDropdown,
+            AmPopover,
+        },
         props: {
             data: {},
             scopeSlotFunc: {},
