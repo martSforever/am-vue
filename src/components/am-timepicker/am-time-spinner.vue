373ce79c0a93c92437cd4e62b9699fc88332f68a
martSforever <marts_forever@163.com>
2018-10-05 17:25:20 +0800

    add time-spinner

---
+++ src/components/am-timepicker/am-time-spinner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="am-time-spinner" style="height: 140px;width: 44px">
+        <am-scrollbar
+            :scroll-x="false"
+            :scrollbar-size="6"
+        >
+            <div class="am-time-spinner-item" v-for="(item,index) in list" :key="index">
+                {{item}}
+            </div>
+        </am-scrollbar>
+    </div>
+</template>
+
+<script>
+    import AmScrollbar from '../am-scrollbar'
+
+    export default {
+        name: "am-time-spinner",
+        components: {
+            AmScrollbar
+        },
+        props: {
+            num: {type: Number, default: 60}
+        },
+        data() {
+            return {
+                list: []
+            }
+        },
+        mounted() {
+            let i = 0
+            while (i < this.num) this.list.push(i++)
+        },
+    }
+</script>
--- src/components/am-timepicker/am-timepicker.vue
+++ src/components/am-timepicker/am-timepicker.vue
@@ -1,35 +1,54 @@
 <template>
     <div class="am-timepicker">
-        <am-dropdown
-            :width="180"
-            :height="180"
+        <am-input ref="input"
+                  @click="currentShow = true"
+                  :suffix-icon="suffixIcon"
+        />
+        <am-popover
+            reference-name="input"
+            parent-name="am-timepicker"
+            v-model="currentShow"
         >
-            <am-input slot="reference"/>
-            <div slot="popover"
-                 class="am-timepicker-content-wrapper"
-            >
+            <div class="am-timepicker-content-wrapper">
+                <div class="am-timepicker-spinner-wrapper">
+                    <am-time-spinner :num="24"/>
+                    <am-time-spinner :num="60"/>
+                    <am-time-spinner :num="60"/>
+                </div>
                 <am-button-group size="small">
-                    <am-button color="error">取消</am-button>
-                    <am-button color="success">确定</am-button>
+                    <am-button color="error" type="none">取消</am-button>
+                    <am-button color="success" type="none">确定</am-button>
                 </am-button-group>
             </div>
-        </am-dropdown>
+        </am-popover>
     </div>
 </template>
 
 <script>
     import AmInput from '../am-input'
-    import AmDropdown from '../am-dropdown'
     import AmButtonGroup from '../am-button/am-button-group'
     import AmButton from '../am-button/am-button'
+    import AmTimeSpinner from './am-time-spinner'
+    import AmPopover from '../am-popover'
 
     export default {
         name: "am-timepicker",
         components: {
             AmInput,
-            AmDropdown,
             AmButton,
             AmButtonGroup,
+            AmTimeSpinner,
+            AmPopover,
+        },
+        props: {
+            show: {type: Boolean, default: false},
+
+            suffixIcon: {type: String, default: 'fas-clock'},
+        },
+        data() {
+            return {
+                currentShow: this.show
+            }
         },
     }
 </script>
