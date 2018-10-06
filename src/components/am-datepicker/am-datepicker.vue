f27f11f2fbefd2517e0f2ea869a3326b83c0b224
martSforever <marts_forever@163.com>
2018-10-06 14:41:10 +0800

    add month pannel

--- src/components/am-datepicker/am-datepicker.vue
+++ src/components/am-datepicker/am-datepicker.vue
@@ -1,9 +1,24 @@
 <template>
     <div class="am-datepicker">
         <am-button @click="$refs.year.updatePosition()">updatePosition</am-button>
-        <am-input :suffix-icon="suffixIcon"/>
+        <am-input :suffix-icon="suffixIcon"
+                  :value="showValue"/>
         <div class="am-datepicker-content-wrapper">
-            <am-year-pannel v-model="currentYear" ref="year"/>
+            <div class="am-datepicker-head">
+                <am-icon icon="fas-angle-double-left"/>
+                <am-icon icon="fas-angle-left"/>
+                <div class="am-datepicker-head-text">
+                    <span>{{currentYear}}</span>-<span>{{zeroize(currentMonth+1)}}</span>-<span>{{zeroize(currentDate)}}</span>
+                </div>
+                <am-icon icon="fas-angle-right"/>
+                <am-icon icon="fas-angle-double-right"/>
+            </div>
+            <div class="am-datepicker-pannel-wrapper">
+                <am-year-pannel v-model="currentYear" ref="year"/>
+            </div>
+            <div class="am-datepicker-pannel-wrapper">
+                <am-month-pannel v-model="currentMonth"/>
+            </div>
         </div>
     </div>
 </template>
@@ -11,12 +26,19 @@
 <script>
     import AmInput from '../am-input'
     import AmYearPannel from "./am-year-pannel";
+    import AmMonthPannel from "./am-month-pannel";
+    import AmIcon from '../am-icon'
+    import AmButton from '../am-button'
+    import {zeroize} from "../../scripts/utils";
 
     export default {
         name: "am-datepicker",
         components: {
+            AmMonthPannel,
             AmYearPannel,
             AmInput,
+            AmIcon,
+            AmButton,
         },
         props: {
             value: {type: Date, default: () => new Date()},
@@ -29,6 +51,14 @@
                 currentMonth: this.value.getMonth(),
                 currentDate: this.value.getDate(),
             }
+        },
+        computed: {
+            showValue() {
+                return `${this.currentYear}-${this.currentMonth + 1}-${this.currentDate}`
+            },
+        },
+        methods: {
+            zeroize,
         },
     }
 </script>
---
+++ src/components/am-datepicker/am-month-pannel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="am-month-pannel">
+        <div class="am-month-pannel-content-wrapper">
+            <div class="am-month-pannel-item-wrapper"
+                 v-for="(item,index) in list"
+                 @click="currentValue = item"
+                 :key="index">
+                <div class="am-month-pannel-item"
+                     :class="{'am-month-pannel-item-active':item===currentValue,'am-month-pannel-item-now':item === nowMonth}">
+                    {{item+1}}月
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "am-month-pannel",
+        props: {
+            value: {},
+        },
+        watch: {
+            value(val) {
+                if (this.currentValue !== val) this.currentValue = val
+            },
+            currentValue(val) {
+                this.$emit('input', val)
+            },
+        },
+        data() {
+            return {
+                list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                currentValue: this.value,
+                nowMonth: new Date().getMonth()
+            }
+        },
+    }
+</script>
