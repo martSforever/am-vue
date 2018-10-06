f0e124211834a80998f6c2dcc2657379826a6255
martSforever <marts_forever@163.com>
2018-10-06 11:20:00 +0800

    datetime picker 基本样式

--- src/components/am-datepicker/am-datepicker.vue
+++ src/components/am-datepicker/am-datepicker.vue
@@ -1,19 +1,33 @@
 <template>
     <div class="am-datepicker">
         <am-input :suffix-icon="suffixIcon"/>
+        <div class="am-datepicker-content-wrapper">
+            <am-year-pannel v-model="currentYear"/>
+        </div>
     </div>
 </template>
 
 <script>
     import AmInput from '../am-input'
+    import AmYearPannel from "./am-year-pannel";
 
     export default {
         name: "am-datepicker",
         components: {
+            AmYearPannel,
             AmInput,
         },
         props: {
-            suffixIcon: {type: String, default: 'fas-datetime'},
+            value: {type: Date, default: () => new Date()},
+            suffixIcon: {type: String, default: 'fas-calendar-alt'},
+        },
+        data() {
+            return {
+                currentValue: this.value,
+                currentYear: this.value.getFullYear(),
+                currentMonth: this.value.getMonth(),
+                currentDate: this.value.getDate(),
+            }
         },
     }
 </script>
---
+++ src/components/am-datepicker/am-year-pannel.vue
@@ -0,0 +1,49 @@
+<template>
+    <am-scrollbar :scroll-x="false"
+                  ref="scrollbar"
+                  class="am-year-pannel">
+        <div class="am-year-pannel-content-wrapper">
+            <div class="am-year-panel-item"
+                 v-for="(item,index) in list"
+                 :key="index"
+            >
+                {{item}}
+            </div>
+        </div>
+    </am-scrollbar>
+</template>
+
+<script>
+    import AmScrollbar from '../am-scrollbar'
+
+    export default {
+        name: "am-year-pannel",
+        components: {
+            AmScrollbar,
+        },
+        props: {
+            value: {},
+        },
+        watch: {
+            value(val) {
+                if (this.currentValue !== val) this.currentValue = val
+            },
+            currentValue(val) {
+                this.$emit('input', val)
+            },
+        },
+        data() {
+            return {
+                currentValue: this.value,
+                list: [],
+            }
+        },
+        mounted() {
+            let start = this.currentValue - 13
+            let num = 27
+            for (let i = start; i < num + start; i++) {
+                this.list.push(i)
+            }
+        },
+    }
+</script>
